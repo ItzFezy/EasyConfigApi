a51edfdb42eaaff8ad28d4e97dd2b34636223832
ItzFezy <148616668+ItzFezy@users.noreply.github.com>
2024-04-30 00:36:42 -0500

    Update Main.java

--- EasyConfigApi/src/main/java/plugins/easyconfigapi/Main.java
+++ EasyConfigApi/src/main/java/plugins/easyconfigapi/Main.java
@@ -1,14 +1,3 @@
 package plugins.easyconfigapi;
-
-import lombok.Getter;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-import plugins.easyconfigapi.API.ECA;
-import plugins.easyconfigapi.Configs.EasyConfig;
-
-import java.io.File;
-
 public final class Main extends JavaPlugin {
 }
